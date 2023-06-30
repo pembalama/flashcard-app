3f927edd2aaafdbb5d72a0bbbcfaa1f154df1f71
pembalama <pemba.t.lama@gmail.com>
2023-06-30 10:46:36 -0400

    correct display, edit, and delete functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Get the necessary DOM elements
+// Get the necessary DOM elements
 const questionInput = document.getElementById('question-input');
 const answerInput = document.getElementById('answer-input');
 const createFlashcardBtn = document.getElementById('create-flashcard-btn');
@@ -7,92 +7,95 @@ const currentFlashcard = document.getElementById('current-flashcard');
 const flashcardCount = document.getElementById('flashcard-count');
 const clearFlashcardsBtn = document.getElementById('clear-flashcards-btn');
 
-//Create an empty array to store the flashcards
+// Create an empty array to store the flashcards
 const flashcards = [];
 
-//function to create a flashcard
+// Function to create a flashcard
 function createFlashcard(question, answer) {
-	//create a flashcard object
+	// Create a flashcard object
 	const flashcard = {
 		question: question,
 		answer: answer,
 	};
 
-	//add flashcard to the array
+	// Add flashcard to the array
 	flashcards.push(flashcard);
 	updateFlashcardCount();
 	saveFlashcards();
 }
 
 let flashcard;
-//function to display flashcard
-function displayFlashcard(question, answer) {
+// Function to display flashcard
+function displayFlashcard(currentIndex) {
 	if (flashcards.length === 0) {
-		//Display a message if no flashcards are available
+		// Display a message if no flashcards are available
 		currentFlashcard.textContent = 'No flashcards available.';
 		return;
 	}
 
-	//randomly select a flashcard index
-	const randomIndex = Math.floor(Math.random() * flashcards.length);
-	const flashcard = flashcards[randomIndex];
-	//Display the flashcard in the HTML
+	flashcard = flashcards[currentIndex];
+	// Display the flashcard in the HTML
 	currentFlashcard.innerHTML = `
         <h2>Question:</h2>
         <p>${flashcard.question}</p>
         <h2>Answer:</h2>
         <p>${flashcard.answer}</p>
-		<button class="edit-flashcard-btn">Edit Flashcard</button>
+        <button class="edit-flashcard-btn">Edit Flashcard</button>
         <button class="delete-flashcard-btn">Delete Flashcard</button>
     `;
 
 	const deleteFlashcardBtn = document.querySelector('.delete-flashcard-btn');
-	deleteFlashcardBtn.addEventListener('click', () => {
-		deleteFlashcard(randomIndex);
-	});
+	deleteFlashcardBtn.removeEventListener('click', handleDeleteFlashcard);
+	deleteFlashcardBtn.addEventListener('click', () =>
+		handleDeleteFlashcard(currentIndex)
+	);
 
 	const editFlashcardBtn = document.querySelector('.edit-flashcard-btn');
-	editFlashcardBtn.addEventListener('click', () => {
-		editFlashcard(randomIndex);
-	});
+	editFlashcardBtn.removeEventListener('click', handleEditFlashcard);
+	editFlashcardBtn.addEventListener('click', handleEditFlashcard);
 
 	saveFlashcards();
 }
 
-//function to delete flashcard
-function deleteFlashcard(index) {
-	flashcards.splice(index, 1);
+function handleDeleteFlashcard(currentIndex) {
+	flashcards.splice(currentIndex, 1);
 	updateFlashcardCount();
-	displayFlashcard();
+	const nextIndex = currentIndex % flashcards.length;
+	displayFlashcard(nextIndex);
 	saveFlashcards();
 }
 
-//function to update the count
+function handleEditFlashcard() {
+	const currentIndex = flashcards.findIndex(card => card === flashcard);
+	editFlashcard(currentIndex);
+}
+
+// Function to update the count
 function updateFlashcardCount() {
 	const count = flashcards.length;
 	flashcardCount.textContent = `Total Flashcards: ${count}`;
 }
 
-//function to clear all flashcards
+// Function to clear all flashcards
 function clearFlashcards() {
 	flashcards.length = 0;
 	updateFlashcardCount();
-	displayFlashcard();
+	displayFlashcard(0);
 	saveFlashcards();
 }
 
-//function to save flashcards in local storage
+// Function to save flashcards in local storage
 function saveFlashcards() {
 	localStorage.setItem('flashcards', JSON.stringify(flashcards));
 }
 
-//function to load flashcards from local storage
+// Function to load flashcards from local storage
 function loadflashcards() {
 	const savedFlashcards = localStorage.getItem('flashcards');
 	if (savedFlashcards) {
 		flashcards.push(...JSON.parse(savedFlashcards));
-		updateFlashcardCount();
 	}
+	updateFlashcardCount();
 }
 
 function editFlashcard(index) {
@@ -104,77 +107,72 @@ function editFlashcard(index) {
 	if (newQuestion !== null && newAnswer !== null) {
 		flashcard.question = newQuestion;
 		flashcard.answer = newAnswer;
-		displayFlashcard();
+		displayFlashcard(index);
 		saveFlashcards();
 	}
 }
 
-function displayPreviousFlashcard() {
+function displayPreviousFlashcard(currentIndex) {
 	if (flashcards.length === 0) {
 		return;
 	}
 
 	const currentFlashcard = document.getElementById('current-flashcard');
-	const currentIndex = flashcards.findIndex(card => card === flashcard);
 	const previousIndex =
 		(currentIndex - 1 + flashcards.length) % flashcards.length;
 	const previousFlashcard = flashcards[previousIndex];
 
-	// currentFlashcard.classList.remove('current-flashcard');
-	displayFlashcard(previousFlashcard);
+	displayFlashcard(previousIndex);
 }
 
-function displayNextFlashcard() {
+function displayNextFlashcard(currentIndex) {
 	if (flashcards.length === 0) {
 		return;
 	}
 
 	const currentFlashcard = document.getElementById('current-flashcard');
-	const currentIndex = flashcards.findIndex(card => card === flashcard);
 	const nextIndex = (currentIndex + 1) % flashcards.length;
 	const nextFlashcard = flashcards[nextIndex];
 
-	// currentFlashcard.classList.remove('current-flashcard');
-	displayFlashcard(nextFlashcard);
+	displayFlashcard(nextIndex);
 }
 
-const editFlashcardBtn = document.querySelector('.edit-flashcard-btn');
-
-//Event Listener for creating a Flashcard
+// Event Listener for creating a Flashcard
 createFlashcardBtn.addEventListener('click', () => {
-	//Get values from the input fields
+	// Get values from the input fields
 	const question = questionInput.value;
 	const answer = answerInput.value;
 
-	//Call a function to create a flashcard
+	// Call a function to create a flashcard
 	createFlashcard(question, answer);
 
-	//Clear the input fields
+	// Clear the input fields
 	questionInput.value = '';
 	answerInput.value = '';
 });
 
-//Event listener for displaying a flashcard
+// Event listener for displaying a flashcard
 displayFlashcardBtn.addEventListener('click', () => {
-	const question = questionInput.value;
-	const answer = answerInput.value;
+	const currentIndex = 0; // Set the initial currentIndex
 
-	//call a function to display the flashcard
-	displayFlashcard(question, answer);
+	// Call a function to display the flashcard
+	displayFlashcard(currentIndex);
 });
 
-//Event listener for clearing all flashcards
+// Event listener for clearing all flashcards
 clearFlashcardsBtn.addEventListener('click', () => {
 	clearFlashcards();
 });
 
 document.addEventListener('keydown', event => {
 	if (event.key === 'ArrowLeft') {
-		//previous flashcard
-		displayPreviousFlashcard();
+		// Previous flashcard
+		const currentIndex = flashcards.findIndex(card => card === flashcard);
+		displayPreviousFlashcard(currentIndex);
 	} else if (event.key === 'ArrowRight') {
-		//Next flashcard
-		displayNextFlashcard();
+		// Next flashcard
+		const currentIndex = flashcards.findIndex(card => card === flashcard);
+		displayNextFlashcard(currentIndex);
 	}
 });
 
